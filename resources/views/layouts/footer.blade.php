dcaffd81f26500bee3694ebb324c5d35bc3d0c8b
Hendrik <drikdoank@gmail.com>
2016-09-27 10:56:20 +0700

    layouts

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,7 +4,7 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 2.3.6
     </div>
-    <strong>Copyright &copy; 2014-2016 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights
+    <strong>Copyright &copy; 2014-2016 <a href="http://almsaeedstudio.com">AppSynthesis Studio</a>.</strong> All rights
     reserved.
   </footer>
   <div class="control-sidebar-bg"></div>
