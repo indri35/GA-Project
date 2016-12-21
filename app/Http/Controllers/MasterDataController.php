a601f329605ef442a0f44c9fef8b5e93c933d33d
Hendrik <drikdoank@gmail.com>
2016-12-21 16:12:40 +0700

    param

--- app/Http/Controllers/MasterDataController.php
+++ app/Http/Controllers/MasterDataController.php
@@ -38,48 +38,47 @@ class MasterDataController extends Controller {
 	 */
 	public function store(Request $request)
 	{
-		$this->validate($request, [
-			'c' => 'required',
-			'i' => 'required',
-			'n' => 'required',
-			'w' => 'required',
-			'o' => 'required',
-			'a' => 'required',
-			'b' => 'required'
-		]);
-
-		$tr = new TranslateClient(); // Default is from 'auto' to 'en'
-		$tr->setSource('en'); // Translate from English
-		$tr->setTarget('id'); // Translate to Indonesian
-
-		$master_datum = new MasterData();
-		$ip = $request->ip();
-		$detail = json_decode(file_get_contents("http://ipinfo.io/{$ip}"));
-		$master_datum->id = $request->input("id");
-		$master_datum->user = "drikdoank@gmail.com";
-		$master_datum->id_aplikasi = 1;
-		$master_datum->ip = $ip;
-		$master_datum->connected_by=$request->input("w");
-        $master_datum->imei = $request->input("i");
-        $master_datum->operator = $request->input("n");
-        $master_datum->os = $request->input("o");
-        $master_datum->click = $request->input("c");
-        $master_datum->view = $request->input("a");
-        $master_datum->type_device = $request->input("b");
-        $master_datum->language = $detail->country;
-        
-		
-		$reg_indo=$tr->translate($detail->region);
-		if($reg_indo=="Jakarta")
-			$reg_indo="Jakarta Raya";
-    
-	    $master_datum->state = $reg_indo;		
-		
-        $master_datum->regional = $detail->city;	
-		$master_datum->loc = $detail->loc;
-						
-		$master_datum->save();
 		$status = true;
+		$master_datum = new MasterData();
+
+		if (isset($request['w']) && isset($request['i'])
+		 && isset($request['n']) && isset($request['o']) 
+		 && isset($request['c'])&& isset($request['a']) 
+		 && isset($request['b'])){
+			$tr = new TranslateClient(); // Default is from 'auto' to 'en'
+			$tr->setSource('en'); // Translate from English
+			$tr->setTarget('id'); // Translate to Indonesian
+
+			$ip = $request->ip();
+			$detail = json_decode(file_get_contents("http://ipinfo.io/{$ip}"));
+			$master_datum->id = $request->input("id");
+			$master_datum->user = "drikdoank@gmail.com";
+			$master_datum->id_aplikasi = 1;
+			$master_datum->ip = $ip;
+			$master_datum->connected_by=$request->input("w");
+			$master_datum->imei = $request->input("i");
+			$master_datum->operator = $request->input("n");
+			$master_datum->os = $request->input("o");
+			$master_datum->click = $request->input("c");
+			$master_datum->view = $request->input("a");
+			$master_datum->type_device = $request->input("b");
+			$master_datum->language = $detail->country;
+			
+			
+			$reg_indo=$tr->translate($detail->region);
+			if($reg_indo=="Jakarta")
+				$reg_indo="Jakarta Raya";
+		
+			$master_datum->state = $reg_indo;		
+			
+			$master_datum->regional = $detail->city;	
+			$master_datum->loc = $detail->loc;
+							
+			$master_datum->save();
+		}else{
+			$status = false;
+			$master_datum = "parameter not complete";
+		}
         return compact('status','master_datum');
 	}
 
