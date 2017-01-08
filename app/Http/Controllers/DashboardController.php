ef31b1a242b79e00e5b5b66390f4b9b75f12a158
Hendrik <drikdoank@gmail.com>
2017-01-08 14:07:20 +0700

    "sdd"

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -165,7 +165,7 @@ class DashboardController extends Controller
         $user = Auth::user();
         if($user->role=='admin'){
             $master_datas = Aplikasi::count();
-            $master_dataa = Count::orderBy('count_click', 'desc')->Where('id_aplikasi',$user->active_app)->paginate(10);
+            $master_dataa = Count::orderBy('count_click', 'desc')->paginate(10);
         }else{
             $master_datas = Aplikasi::Where('user',$user->email)->count();
             $master_dataa = Usercount::orderBy('count_click', 'desc')->Where('user',$user->email)->Where('id_aplikasi',$user->active_app)->paginate(10);            
