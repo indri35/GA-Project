c3980df6dd862c582e970096e065b4c7f328b045
Hendrik <drikdoank@gmail.com>
2017-01-28 19:12:08 +0700

    hghg

--- resources/views/layouts/chartdashboard.blade.php
+++ resources/views/layouts/chartdashboard.blade.php
@@ -328,14 +328,13 @@
             }
           var datanya = google.visualization.arrayToDataTable(datachart);
 
-         var options = {
-          region: 'ID',
-          displayMode: 'markers',
-          colorAxis: {colors: ['green', 'blue']}
-        };
+          var options = {};
+          options['region'] = 'ID';
+          //options['colors'] = [0xFF8747, 0xFFB581, 0xc06000]; //orange colors
+          options['dataMode'] = 'markers';
 
           var container = document.getElementById('map_city');
-          var geomap = new google.visualization.GeoChart(container);
+          var geomap = new google.visualization.GeoMap(container);
           geomap.draw(datanya, options);
           }
         });
