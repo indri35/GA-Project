55c1d9bbfe15cae02f2a46ea34bb00db55b85c88
Hendrik <drikdoank@gmail.com>
2017-02-02 20:24:48 +0700

    tab songs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.app')
-
-@section('content')
+<!DOCTYPE html>
+<html>
+@include('layouts.header')
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
