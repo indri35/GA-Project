dfd9d8bb57090bcd21bba868d98d25d82057c982
Hendrik <drikdoank@gmail.com>
2017-04-22 12:58:57 +0700

    d

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -33,6 +33,24 @@
           </div>
         </div>
         <!-- ./col -->
+                <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-red">
+            <div class="inner">
+              <h3>{{$apps}}</h3> 
+              <p>Regsitered Apps</p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-table"></i>
+            </div>
+            <a href="{{ url('/master-data') }}" class="small-box-footer">
+              More info <i class="fa fa-arrow-circle-right"></i>
+            </a>
+          </div>
+        </div>
+        <!-- ./col -->
+      </div>
+
         <div class="col-lg-3 col-xs-6">
           <!-- small box -->
           <div class="small-box bg-green">
@@ -71,23 +89,6 @@
           </div>
         </div>
         <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-red">
-            <div class="inner">
-              <h3>{{$apps}}</h3> 
-              <p>Regsitered Apps</p>
-            </div>
-            <div class="icon">
-              <i class="fa fa-table"></i>
-            </div>
-            <a href="{{ url('/master-data') }}" class="small-box-footer">
-              More info <i class="fa fa-arrow-circle-right"></i>
-            </a>
-          </div>
-        </div>
-        <!-- ./col -->
-      </div>
       <!-- /.row -->
       <div class="row">
         <div class="col-md-6">
